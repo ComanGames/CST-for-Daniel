ad8f9dc32293b68f30dec8a80a7da54c11b949c2
GeorgeWolf <alfaa.gen@gmail.com>
2015-10-15 23:50:19 -0400

    Bad Idea

---
+++ Strategy/ChartSlopeTrader.UI.cs
@@ -0,0 +1,22 @@
+﻿#region Using declarations
+
+#endregion
+
+// ReSharper disable once CheckNamespace
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NinjaTrader.Strategy
+{
+	public partial class ChartSlopeTrader
+	{
+	#region VS2010 Controls Paste
+
+		public readonly Color _activateColor = Color.DarkBlue;
+		private readonly Color _enabledColor = Color.ForestGreen;
+		public readonly Color _disabledColor = Color.LightCoral;
+
+		#endregion
+	}
+}
---
+++ Strategy/SomeOtherClass.cs
@@ -0,0 +1,9 @@
+using System.Windows.Forms;
+
+namespace NinjaTrader.Strategy
+{
+	public class SomeOtherClass
+	{
+		private Button _buttonClosePosition;
+	}
+}
